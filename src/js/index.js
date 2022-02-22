cdbc6eeb3c6b5ce4391f342d3ae23539331aaff8
Christiano0407 <placebo0704@gmail.com>
2022-02-21 20:29:53 -0600

    player modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
+// Modules ==>
+import playOne from "./modules/app.js";
+import { twoPlay, plugin } from "./modules/app.js";
+console.log(playOne());
+console.log(twoPlay());
+console.log(plugin());
+
 // =================== Const
 const btnMenu = document.querySelector(".menu__active");
 const listMedia = document.querySelector(".nav__media");
---
+++ src/js/modules/app.js
@@ -0,0 +1,12 @@
+// Modules
+export default function playOne() {
+  return "Play One";
+}
+
+export const twoPlay = () => {
+  return "I´m Two Play";
+};
+
+export const plugin = () => {
+  return "Plugin";
+};
