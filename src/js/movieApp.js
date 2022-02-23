7f8256cb3ea82713e8f020d0712bdc699b478e59
Christiano0407 <placebo0704@gmail.com>
2022-02-23 13:26:53 -0600

    navegation querie

---
+++ src/js/movieApp.js
@@ -0,0 +1,8 @@
+// Hamburger Menu ====>
+const header = document.getElementById("header");
+const navList = document.getElementById("navList");
+const burgerMenu = document.getElementById("burger");
+let hamburgerOpen = false;
+
+// ==Event ===================>>
+burgerMenu.addEventListener("click", () => {});
