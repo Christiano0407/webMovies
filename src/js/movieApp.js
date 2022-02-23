e7acc9f51c809bc6e92df2d238290508f491d8e1
Christiano0407 <placebo0704@gmail.com>
2022-02-23 16:14:56 -0600

    list

--- src/js/movieApp.js
+++ src/js/movieApp.js
@@ -1,8 +1,19 @@
 // Hamburger Menu ====>
 const header = document.getElementById("header");
 const navList = document.getElementById("navList");
+const list = document.getElementById(".list");
 const burgerMenu = document.getElementById("burger");
+
 let hamburgerOpen = false;
 
 // ==Event ===================>>
-burgerMenu.addEventListener("click", () => {});
+burgerMenu.addEventListener("click", () => {
+  console.log("active");
+  if (navList.classList.contains("active")) {
+    navList.classList.add("hidden");
+    navList.classList.remove("active");
+  } else {
+    navList.classList.add("active");
+    navList.classList.remove("hidden");
+  }
+});
