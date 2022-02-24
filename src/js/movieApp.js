a3cfffa4330f5667b116b6aa6315a597927bbb82
Christiano0407 <placebo0704@gmail.com>
2022-02-24 12:52:27 -0600

    all first api movies

--- src/js/movieApp.js
+++ src/js/movieApp.js
@@ -12,3 +12,39 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("is-active");
   navList.classList.toggle("active");
 });
+
+// ============ API Movies ============
+const addMovies = async () => {
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f&language=es-Mx"
+    );
+
+    console.log(response);
+    //> Status >
+    if (response.status === 200) {
+      const data = await response.json();
+
+      let movies = " ";
+      data.results.forEach((movie) => {
+        movies += `
+        <div class="movies">
+         <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+         <h1>${movie.title}</h1> 
+          </div>`;
+      });
+      // Container =>
+      document.getElementById("container").innerHTML = movies;
+    } else if (response.data.status === 400) {
+      console.log("Error! Page not exist");
+    } else {
+      console.log("Movies is not exist");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // => Llamar a container ===>
+};
+
+addMovies();
