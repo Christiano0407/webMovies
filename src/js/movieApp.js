427f21de0f1966e58575812da26fbd8fd31f1330
Christiano0407 <placebo0704@gmail.com>
2022-02-24 08:39:41 -0600

    active btn-burger

--- src/js/movieApp.js
+++ src/js/movieApp.js
@@ -1,12 +1,16 @@
 // Hamburger Menu ====>
 const header = document.getElementById("header");
-const navList = document.getElementById("navList");
-const list = document.getElementById(".list");
-const menuToggle = document.getElementById("menu-toggle");
+const navList = document.getElementById("navList"); // Menu y nav-list
+const list = document.getElementById(".list"); // Menu List e links
+const menuToggle = document.getElementById("menu-toggle"); //Mobile-Menu
 
 let hamburgerOpen = false;
 
 // ==Event ===================>>
-burgerMenu.addEventListener("click", () => {
+menuToggle.addEventListener("click", () => {
   console.log("active");
+  menuToggle.classList.toggle("is-active");
+  menuToggle.classList.toggle("active");
+
+  
 });
