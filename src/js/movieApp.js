159bbc66b2a76870d9bc97b28ae2c8e4cef66aae
Christiano0407 <placebo0704@gmail.com>
2022-02-24 07:37:46 -0600

    change color and menu

--- src/js/movieApp.js
+++ src/js/movieApp.js
@@ -2,18 +2,11 @@
 const header = document.getElementById("header");
 const navList = document.getElementById("navList");
 const list = document.getElementById(".list");
-const burgerMenu = document.getElementById("burger");
+const menuToggle = document.getElementById("menu-toggle");
 
 let hamburgerOpen = false;
 
 // ==Event ===================>>
 burgerMenu.addEventListener("click", () => {
   console.log("active");
-  if (navList.classList.contains("active")) {
-    navList.classList.add("hidden");
-    navList.classList.remove("active");
-  } else {
-    navList.classList.add("active");
-    navList.classList.remove("hidden");
-  }
 });
