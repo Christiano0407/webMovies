5325bdf0e0651102fef2eceae5937dc6d1336850
Christiano0407 <placebo0704@gmail.com>
2022-03-02 16:26:49 -0600

    add carousel

--- movieWeb/movieWebJs.js
+++ movieWeb/movieWebJs.js
@@ -1,1 +1,26 @@
 console.log("Movies App");
+// ==> Swiper Carousel {
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidePerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    760: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: 8,
+      spaceBetween: 40,
+    },
+  },
+});
